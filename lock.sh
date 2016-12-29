6d57faf956f235cf7d109dad073871a9c3ab3d9c
jacobernst98 <jacobaernst@gmail.com>
2016-12-29 13:50:32 -0500

    changed commented lines

--- lock.sh
+++ lock.sh
@@ -8,9 +8,9 @@
 LOCK=/tmp/pixellock.png
 #Uncomment one of the next 3 lines for diffrent pixelation types.
 ###
-#SCALE1="10%" SCALE2="1000%"
+SCALE1="10%" SCALE2="1000%"
 #SCALE1="20%" SCALE2="500%"
-SCALE1="40%" SCALE2="250%"
+#SCALE1="40%" SCALE2="250%"
 ###
 SNAPSHOT="scrot $LOCK"
 
